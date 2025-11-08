5922b7c237f55562296d3891c729f83c05cc49f8
Milan <76396495+themilan1337@users.noreply.github.com>
2025-11-08 17:42:54 +0500

    analytics added

--- app/app.vue
+++ app/app.vue
@@ -2,6 +2,10 @@
   <div class="bg-white">
     <NuxtLayout>
       <NuxtPage />
+      <Analytics/>
     </NuxtLayout>
   </div>
 </template>
+<script setup lang="ts">
+import { Analytics } from '@vercel/analytics/nuxt'
+</script>
