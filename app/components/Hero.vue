48b91619165484e1c9e8da1caf663509eae7c8f3
Milan <76396495+themilan1337@users.noreply.github.com>
2025-11-08 14:24:38 +0500

    hotfix

--- app/components/Hero.vue
+++ app/components/Hero.vue
@@ -1,16 +1,16 @@
 <template>
 
 <div class="max-w-xl">
-  <p
-    class="text-xs relative font-semibold uppercase tracking-wide text-zinc-500 dark:text-zinc-400 text-blue-500 dark:text-blue-400"
+  <!-- <p
+    class="text-xs gs relative font-semibold uppercase tracking-wide text-zinc-500 dark:text-zinc-400 text-blue-500 dark:text-blue-400"
   >
     Молодёжный стартап из Казахстана
-    <a href="#_" class="relative text-blue-500 dark:text-blue-400">
+    <a href="#_" class="relative text-blue-500 underline dark:text-blue-400">
       <span class="absolute inset-0" aria-hidden="true"></span> Узнать больше
     </a>
-  </p>
+  </p> -->
   <h2
-    class="text-2xl md:text-3xl lg:text-4xl mt-12 font-semibold tracking-tight text-zinc-900 dark:text-white lg:text-balance"
+    class="text-2xl gs md:text-3xl lg:text-4xl mt-12 font-semibold tracking-tight text-zinc-900 dark:text-white lg:text-balance"
   >
     Юридическая помощь доступна каждому с AdiletAI
   </h2>
@@ -19,12 +19,12 @@
   </p>
   <div class="flex flex-wrap items-center mt-8 gap-2">
     <NuxtLink to="https://app.adiletai.com/"
-      class="relative flex items-center justify-center text-center font-medium transition-colors duration-200 ease-in-out select-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:z-10 justify-center rounded-md text-white bg-blue-700 outline outline-blue-700 hover:bg-blue-600 focus-visible:outline-blue-600 dark:bg-blue-600 dark:text-zinc-100 dark:outline-blue-600 dark:hover:bg-blue-700 dark:focus-visible:outline-blue-500 h-9 px-4 text-sm"
+      class="relative flex gs items-center justify-center text-center font-medium transition-colors duration-200 ease-in-out select-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:z-10 justify-center rounded-md text-white bg-blue-700 outline outline-blue-700 hover:bg-blue-600 focus-visible:outline-blue-600 dark:bg-blue-600 dark:text-zinc-100 dark:outline-blue-600 dark:hover:bg-blue-700 dark:focus-visible:outline-blue-500 h-9 px-4 text-sm"
     >
       Начать сейчас
     </NuxtLink>
     <NuxtLink to="#"
-      class="relative flex items-center justify-center text-center font-medium transition-colors duration-200 ease-in-out select-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:z-10 justify-center rounded-md text-zinc-600 bg-zinc-50 outline outline-zinc-100 hover:bg-zinc-200 focus-visible:outline-zinc-600 dark:text-zinc-100 dark:bg-zinc-800 dark:outline-zinc-800 dark:hover:bg-zinc-700 dark:focus-visible:outline-zinc-700 h-9 px-4 text-sm"
+      class="relative flex gs items-center justify-center text-center font-medium transition-colors duration-200 ease-in-out select-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:z-10 justify-center rounded-md text-zinc-600 bg-zinc-50 outline outline-zinc-100 hover:bg-zinc-200 focus-visible:outline-zinc-600 dark:text-zinc-100 dark:bg-zinc-800 dark:outline-zinc-800 dark:hover:bg-zinc-700 dark:focus-visible:outline-zinc-700 h-9 px-4 text-sm"
     >
       Наши возможности
     </NuxtLink>
@@ -33,25 +33,25 @@
 <div class="mt-12 grid grid-cols-2 gap-4 sm:grid-cols-2 md:grid-cols-4">
   <div class="flex flex-col">
     <p class="text-base font-medium text-zinc-900 dark:text-white">+83%</p>
-    <h3 class="text-sm font-medium text-zinc-500 dark:text-zinc-400">
+    <h3 class="gs text-sm font-medium text-zinc-500 dark:text-zinc-400">
       Точность выше ChatGPT
     </h3>
   </div>
   <div class="flex flex-col">
     <p class="text-base font-medium text-zinc-900 dark:text-white">16-17ч.</p>
-    <h3 class="text-sm font-medium text-zinc-500 dark:text-zinc-400">
+    <h3 class="gs text-sm font-medium text-zinc-500 dark:text-zinc-400">
       Экономия в неделю
     </h3>
   </div>
   <div class="flex flex-col">
     <p class="text-base font-medium text-zinc-900 dark:text-white">RAG</p>
-    <h3 class="text-sm font-medium text-zinc-500 dark:text-zinc-400">
+    <h3 class="gs text-sm font-medium text-zinc-500 dark:text-zinc-400">
       Технология проверки
     </h3>
   </div>
   <div class="flex flex-col">
     <p class="text-base font-medium text-zinc-900 dark:text-white">10+</p>
-    <h3 class="text-sm font-medium text-zinc-500 dark:text-zinc-400">
+    <h3 class="gs text-sm font-medium text-zinc-500 dark:text-zinc-400">
       Пользователей
     </h3>
   </div>
