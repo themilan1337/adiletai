2003ca94a59a7b772bdd95d6c3880ca40a3edd20
Milan <76396495+themilan1337@users.noreply.github.com>
2026-02-03 13:53:06 +0500

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
+  ssr: true,
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ['./app/assets/css/main.css'],
